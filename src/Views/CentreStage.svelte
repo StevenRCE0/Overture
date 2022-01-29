653d6451559a481248474061c07405ddcaf3b468
StevenRCE0 <40051361+StevenRCE0@users.noreply.github.com>
2022-01-29 19:18:38 +0800

    Fixup for weirdo

--- src/Views/CentreStage.svelte
+++ src/Views/CentreStage.svelte
@@ -2,7 +2,6 @@
     import * as THREE from "three"
     import { onMount } from "svelte"
     import { watchResize } from "svelte-watch-resize"
-    import { t } from "svelte-canvas"
 
     let anchor: HTMLElement
     let lever: { [index: string]: number } = {
@@ -23,17 +22,10 @@
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.7)
     let init = false
 
-    function timeFunction() {
-        const x = Math.sin(t)
-        const y = Math.cos(t)
-        const z = Math.sin(t * 0.5)
-        return new THREE.Vector3(x, y, z)
-    }
-
     function render() {
         console.log(lever)
         const Spotlight = new THREE.SpotLight(0xffffff, 0.5)
-        
+
         Spotlight.translateOnAxis(
             new THREE.Vector3(lever.x, lever.y, lever.z),
             20
@@ -58,14 +50,18 @@
         ceiling.rotateX(Math.PI / 2)
         leftWall.translate(-window.innerWidth / 3, 0, -100)
         rightWall.translate(window.innerWidth / 3, 0, -100)
-        floor.translate(0, -window.innerHeight / 3, 0)
-        ceiling.translate(0, window.innerHeight / 3, 0)
+        floor.translate(0, -window.innerHeight / 3, -100)
+        ceiling.translate(0, window.innerHeight / 3, -100)
         const wallMaterial = new THREE.MeshStandardMaterial({
             metalness: 0,
             roughness: 0.6,
             color: 0xe3e3e3,
         })
-        wallBack.translate(0, 0, -300)
+        wallBack.translate(
+            0,
+            0,
+            -Math.min(window.innerHeight, window.innerWidth) / 2
+        )
         const wallBackMesh = new THREE.Mesh(wallBack, wallMaterial)
         const leftWallMesh = new THREE.Mesh(leftWall, wallMaterial)
         const rightWallMesh = new THREE.Mesh(rightWall, wallMaterial)
