43f1cd9f3aca60de174a90892e100cd1b711d7bb
StevenRCE0 <StevenRCE0@users.noreply.github.com>
2022-02-04 02:39:13 +0800

    Something

--- src/Views/Books.svelte
+++ src/Views/Books.svelte
@@ -118,10 +118,10 @@
                     )
                     figure.outOf.translateX(bookIndex * innerWidth * Spacer)
 
-                    figure.digitCurrent.translateX(-45)
+                    figure.digitCurrent.translateX(-30)
                     figure.digitCurrent.translateY(75)
                     figure.digitCurrent.translateZ(-75)
-                    figure.digitTotal.translateX(45)
+                    figure.digitTotal.translateX(30)
                     figure.digitTotal.translateY(75)
                     figure.digitTotal.translateZ(-75)
                     figure.outOf.translateY(90)
